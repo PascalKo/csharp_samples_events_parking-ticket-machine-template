7586e53647ca6f30908a63c0c73f25aa1cc28a60
Pascal Königshofer <pascalkoenigshofer@gmx.at>
2020-01-22 13:26:00 +0100

    Implemented methods

--- source/ParkingTicketMachine.Core/SlotMachine.cs
+++ source/ParkingTicketMachine.Core/SlotMachine.cs
@@ -11,11 +11,71 @@ namespace ParkingTicketMachine.Core
         private string _name;
         private int _slot;
         private Ticket _ticket;
-        private double _minute = 0;
+        private double _minutes = 0;
 
         public DateTime ValidUntil { get;private set; }
 
+        public SlotMachine(string name)
+        {
+            _name = name;
+        }
 
+        public void Print()
+        {
+            _ticket = new Ticket();
+            _ticket.Amount = _slot;
+            _ticket.Description = _name;
+            _slot = 0;
+            LogTicket?.Invoke(this, _ticket);
+        }
 
+        public void Cancel()
+        {
+            _slot = 0;
+        }
+
+        public string Insert(int coin)
+        {
+            _slot += coin;
+            string validTime = String.Empty;
+            if (_slot >= _minAmount)
+            {
+                if (_slot >= 50 && _slot < 100)
+                {
+                    _minutes = 30;
+                }
+                else if (_slot >= 100 && _slot < 200)
+                {
+                    _minutes = 60;
+                }
+                else if (_slot >= 200)
+                {
+                    _minutes = 90;
+                }
+
+                if (FastClock.Instance.Time.TimeOfDay < _startTime.TimeOfDay)
+                {
+                    ValidUntil = FastClock.Instance.Time.AddMinutes(_minutes + _startTime.TimeOfDay.TotalMinutes);
+                }
+                else if (FastClock.Instance.Time.TimeOfDay > _endTime.TimeOfDay)
+                {
+                    ValidUntil = FastClock.Instance.Time.AddDays(1);
+                    ValidUntil = FastClock.Instance.Time.AddMinutes(_minutes + _startTime.TimeOfDay.TotalMinutes);
+                }
+                else
+                {
+                    ValidUntil = FastClock.Instance.Time.AddMinutes(_minutes);
+                    if (ValidUntil > _endTime)
+                    {
+                        TimeSpan timeSpan = ValidUntil.TimeOfDay - _endTime.TimeOfDay;
+                        ValidUntil = FastClock.Instance.Time.AddDays(1);
+                        ValidUntil = ValidUntil.Date.AddMinutes(timeSpan.TotalMinutes + _startTime.TimeOfDay.TotalMinutes);
+                    }
+                }
+
+                validTime = ValidUntil.ToShortTimeString();
+            }
+            return validTime;
+        }
     }
 }
