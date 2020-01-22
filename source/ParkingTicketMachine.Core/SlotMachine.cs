d3c3063a4cd2e8bc154d1e159ed41775d65445e9
Pascal Königshofer <pascalkoenigshofer@gmx.at>
2020-01-22 12:53:54 +0100

    Implemented fields

--- source/ParkingTicketMachine.Core/SlotMachine.cs
+++ source/ParkingTicketMachine.Core/SlotMachine.cs
@@ -4,5 +4,18 @@ namespace ParkingTicketMachine.Core
 {
     public class SlotMachine
     {
+        public event EventHandler<Ticket> LogTicket;
+        private DateTime _startTime=DateTime.Parse("08:00");
+        private DateTime _endTime=DateTime.Parse("18:00");
+        private int _minAmount=50;
+        private string _name;
+        private int _slot;
+        private Ticket _ticket;
+        private double _minute = 0;
+
+        public DateTime ValidUntil { get;private set; }
+
+
+
     }
 }
