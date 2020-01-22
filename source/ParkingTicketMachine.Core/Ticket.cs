3380023a8df8abd9399d51798303272de05b72e9
Pascal Königshofer <pascalkoenigshofer@gmx.at>
2020-01-22 12:45:26 +0100

    Implemented Ticket

--- source/ParkingTicketMachine.Core/Ticket.cs
+++ source/ParkingTicketMachine.Core/Ticket.cs
@@ -4,5 +4,13 @@ namespace ParkingTicketMachine.Core
 {
     public class Ticket
     {
+        public int Amount { get; set; }
+        public string Description { get; set; }
+
+        public Ticket()
+        {
+
+        }
+
     }
 }
